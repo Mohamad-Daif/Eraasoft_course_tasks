ef5eab03d506d53ffafc2e7bbfb62215dc4b6eb9
Daif <mohamadamgad09@gmail.com>
2026-05-14 23:17:40 +0300

    remove un needed classes.

--- Final Servlet Project/ItemsApp/src/main/java/repo/user/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package repo.user;
-
-import java.sql.ResultSet;
-
-public interface UserRepo {
-
-    ResultSet login(String username, String password);
-    void signup(String username, String password);
-}
